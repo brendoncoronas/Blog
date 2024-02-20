326e27061cb8c2f19410c15536e2e83371a1a595
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2024-02-20 16:22:01 -0300

    Add files via upload

---
+++ djangoapp/site_setup/context_processors.py
@@ -0,0 +1,15 @@
+from site_setup.models import SiteSetup
+
+
+def context_processors_example(request):
+    return {
+        'example': 'veio do context processor (example)'
+    }
+
+
+def site_setup(request):
+    setup = SiteSetup.objects.order_by('-d').first()
+
+    return {
+        'site_setup': setup,
+    }
